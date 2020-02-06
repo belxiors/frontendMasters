9b8e01983eef94815d51a40dbbd66177f0ff67d7
Ricardo BELCHIOR <ricardo.belchior.e@thalesdigital.io>
2020-02-06 21:52:15 +0000

    solution for prime exercise

--- primer/ex.js
+++ primer/ex.js
@@ -1,13 +1,19 @@
-// TODO: define addFavoriteBook(..) function
-
-// TODO: define printFavoriteBooks() function
-
 var favoriteBooks = [];
+
+function addFavoriteBook(bookName) {
+  if (!bookName.includes('Great')) {
+      favoriteBooks.push(bookName);
+  }
+}
+
+function printFavoriteBooks() {
+  console.log(`Favorite books: ${favoriteBooks.length}`);
+  favoriteBooks.forEach(favoriteBook => console.log(favoriteBook));
+}
 
 addFavoriteBook("A Song of Ice and Fire");
 addFavoriteBook("The Great Gatsby");
 addFavoriteBook("Crime & Punishment");
 addFavoriteBook("Great Expectations");
 addFavoriteBook("You Don't Know JS");
-
-// TODO: print out favorite books
+printFavoriteBooks();
